8897848a06f5b5dfb8bab2625909401357ccf394
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-06-09 16:58:14 +0300

    Splash screen added

---
+++ app/src/main/java/com/example/jua_kaligo/Splash_Screen.java
@@ -0,0 +1,32 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent welcomeIntent = new Intent(Splash_Screen.this, MainActivity.class);
+                    startActivity(welcomeIntent);
+                }
+
+            }
+
+        };
+        thread.start();
+    }
+}
