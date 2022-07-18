6599db5f09efcda286d11f6f7558d512f0b760f6
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-18 17:45:10 +0300

    vendor signup upgrade

--- app/src/main/java/com/example/jua_kaligo/ChooseRole.java
+++ app/src/main/java/com/example/jua_kaligo/ChooseRole.java
@@ -5,22 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class ChooseRole extends AppCompatActivity {
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_role);
+
+
     }
 
     public void customermainActivity(View view) {
         Intent intent = new Intent(ChooseRole.this, MainActivity.class);
         startActivity(intent);
+
+
     }
 
     public void vendorsMainactivity(View view) {
-        Intent intent = new Intent(ChooseRole.this, MainActivity.class);
+        Intent intent = new Intent(ChooseRole.this, vendorReg.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/jua_kaligo/vendorReg.java
+++ app/src/main/java/com/example/jua_kaligo/vendorReg.java
@@ -3,34 +3,26 @@ package com.example.jua_kaligo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
-
-
-public class MainActivity extends AppCompatActivity {
+public class vendorReg extends AppCompatActivity {
     private Spinner spinner, spinner2;
-    private TextView typeofBusiness, idnumber;
-    private MaterialButton customerB, vendorB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_vendor_reg);
 
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
-        idnumber = findViewById(R.id.idnumber);
-        typeofBusiness = findViewById(R.id.typeofbusiness);
-        customerB = findViewById(R.id.customerButton);
-        vendorB = findViewById(R.id.vendorButton);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.region, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.business, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
     }
 }
