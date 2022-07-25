b5debf3be036553d251472f00ff5ed91821957df
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-25 14:44:54 +0300

    flutterwave integrated

--- app/src/main/java/com/example/jua_kaligo/Payment.java
+++ app/src/main/java/com/example/jua_kaligo/Payment.java
@@ -3,6 +3,10 @@ package com.example.jua_kaligo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.flutterwave.raveandroid.RaveUiManager;
+
 
 public class Payment extends AppCompatActivity {
 
@@ -11,6 +15,11 @@ public class Payment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        //RaveUiManager =
+
+    }
+
+    public void paybutton(View view) {
 
     }
 }
