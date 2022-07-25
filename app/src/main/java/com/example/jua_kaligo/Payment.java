43ff6b85a7530dc2b3983c99b69ce23d57b4a03f
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-25 12:00:17 +0300

    payment module

---
+++ app/src/main/java/com/example/jua_kaligo/Payment.java
@@ -0,0 +1,16 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+
+    }
+}
