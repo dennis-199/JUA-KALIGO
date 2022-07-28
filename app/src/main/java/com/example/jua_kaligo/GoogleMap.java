0dc80e4b3f53f4a3b00a01b97adff9fda979ef47
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-28 18:48:36 +0300

    google map activity

---
+++ app/src/main/java/com/example/jua_kaligo/GoogleMap.java
@@ -0,0 +1,14 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoogleMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map);
+    }
+}
