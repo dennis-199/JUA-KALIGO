d7fe72d43f94851fe4f41537fc4c5d590dba3aee
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-28 19:18:23 +0300

    google map more

---
+++ app/src/main/java/com/example/jua_kaligo/GoogleMapActivity.java
@@ -0,0 +1,22 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map);
+
+
+    }
+}
