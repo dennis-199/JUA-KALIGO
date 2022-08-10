264a84a9cace991c18fe634d7110097e82b5af10
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-10 21:15:21 +0300

    smooth transition between layouts

--- app/src/main/java/com/example/jua_kaligo/VendorInformation.java
+++ app/src/main/java/com/example/jua_kaligo/VendorInformation.java
@@ -2,13 +2,41 @@ package com.example.jua_kaligo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.hbb20.CountryCodePicker;
 
 public class VendorInformation extends AppCompatActivity {
+    Spinner spinner, spinner2;
+    TextInputEditText text;
+    EditText phone, fullname, IDnumber;
+    CountryCodePicker countryCodePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_information);
+        phone = findViewById(R.id.phone_number);
+        fullname = findViewById(R.id.fullName);
+        countryCodePicker = findViewById(R.id.country_code_picker);
+        IDnumber = findViewById(R.id.idnumber);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+    }
+
+    public void verifyOTP(View view) {
+
+        //String _getUserenteredphonenumber = phone.getText().toString().trim();
+        //String _getPhoneNumber = "+"+countryCodePicker.getFullNumber()+_getUserenteredphonenumber;
+        Intent intent = new Intent(VendorInformation.this, Verification.class);
+        startActivity(intent);
+
+
     }
 }
--- app/src/main/java/com/example/jua_kaligo/vendorReg.java
+++ app/src/main/java/com/example/jua_kaligo/vendorReg.java
@@ -15,18 +15,15 @@ import com.hbb20.CountryCodePicker;
 public class vendorReg extends AppCompatActivity {
     private Spinner spinner, spinner2;
     TextInputEditText text;
-    EditText phone, fullname, IDnumber;
-    CountryCodePicker countryCodePicker;
+    //EditText phone, fullname, IDnumber;
+    //CountryCodePicker countryCodePicker;
     //Spinner spinnerLocation, Spinnertypeofbusiness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_reg);
-        phone = findViewById(R.id.phone_number);
-        fullname = findViewById(R.id.fullName);
-        countryCodePicker = findViewById(R.id.country_code_picker);
-        IDnumber = findViewById(R.id.idnumber);
+
 
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
@@ -48,9 +45,8 @@ public class vendorReg extends AppCompatActivity {
 
         //}
 
-        String _getUserenteredphonenumber = phone.getText().toString().trim();
-        String _getPhoneNumber = "+"+countryCodePicker.getFullNumber()+_getUserenteredphonenumber;
-        Intent intent = new Intent(vendorReg.this, Verification.class);
+
+        Intent intent = new Intent(vendorReg.this, VendorInformation.class);
         // passing fields to the next activity
         //intent.putExtra("fullname", fullname);
         // start activity
