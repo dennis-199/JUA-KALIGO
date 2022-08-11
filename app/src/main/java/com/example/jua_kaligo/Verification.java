49560af8935fd82e91b2362358433601780935c8
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-11 21:55:53 +0300

    otp ver

--- app/src/main/java/com/example/jua_kaligo/Verification.java
+++ app/src/main/java/com/example/jua_kaligo/Verification.java
@@ -1,13 +1,27 @@
 package com.example.jua_kaligo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.chaos.view.PinView;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Verification extends AppCompatActivity {
     PinView pinfromUser;
+    String codebySystem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +29,40 @@ public class Verification extends AppCompatActivity {
         setContentView(R.layout.activity_verification);
         pinfromUser = findViewById(R.id.pin_view);
 
+
         String _phoneNo = getIntent().getStringExtra("phoneNo");
 
         sendVerificationCodeToUser(_phoneNo);
     }
 
     private void sendVerificationCodeToUser(String phoneNo) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phoneNo,     // Phone number to verify
+                        60,
+                        TimeUnit.SECONDS,// Timeout and unit
+                (Activity) TaskExecutors.MAIN_THREAD,                // Activity (for callback binding)
+                        mCallbacks);        // OnVerificationStateChangedCallbacks
+
 
     }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            codebySystem = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(Verification.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
 }
