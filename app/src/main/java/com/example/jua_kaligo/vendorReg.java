cd8fa8a388ce495f7270fccbcbfcb0fd18a1f095
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-25 13:30:33 +0300

    checking if field is filled

--- app/src/main/java/com/example/jua_kaligo/vendorReg.java
+++ app/src/main/java/com/example/jua_kaligo/vendorReg.java
@@ -15,9 +15,9 @@ import com.hbb20.CountryCodePicker;
 public class vendorReg extends AppCompatActivity {
     private Spinner spinner, spinner2;
     TextInputEditText text;
-    //EditText phone, fullname, IDnumber;
+    EditText phone, fullname, IDnumber;
     //CountryCodePicker countryCodePicker;
-    //Spinner spinnerLocation, Spinnertypeofbusiness;
+    Spinner spinnerLocation, Spinnertypeofbusiness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +25,9 @@ public class vendorReg extends AppCompatActivity {
         setContentView(R.layout.activity_vendor_reg);
 
 
+        phone = (EditText) findViewById(R.id.phoneNo);
+        fullname = (EditText) findViewById(R.id.fullName);
+        IDnumber = (EditText) findViewById(R.id.idnumber);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
 
@@ -46,10 +49,36 @@ public class vendorReg extends AppCompatActivity {
         //}
 
 
-        Intent intent = new Intent(vendorReg.this, VendorInformation.class);
         // passing fields to the next activity
         //intent.putExtra("fullname", fullname);
         // start activity
+        String fullN = fullname.getText().toString();
+        String phoneN = phone.getText().toString();
+        String IDNumber = IDnumber.getText().toString();
+        //String region = spinner.toString();
+
+        if(fullN.isEmpty()){
+            fullname.setError("Fullname is required ");
+            fullname.requestFocus();
+            return;
+        }
+        if(phoneN.isEmpty()){
+            phone.setError("Phone number is required ");
+            phone.requestFocus();
+            return;
+        }
+        if(IDNumber.isEmpty()){
+            IDnumber.setError("ID number is required ");
+            IDnumber.requestFocus();
+            return;
+        }
+
+        // move to next activity
+        Intent intent = new Intent(vendorReg.this, VendorInformation.class);
+        intent.putExtra("FullName", fullN);
+        intent.putExtra("PhoneNumber",phoneN);
+        intent.putExtra("ID_NUMBER",IDNumber);
+
         startActivity(intent);
 
     }
