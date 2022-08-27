270ef219a4877c1f00268ea917e0be0474a158fa
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-27 13:38:56 +0300

    done for today

--- app/src/main/java/com/example/jua_kaligo/MainScreen.java
+++ app/src/main/java/com/example/jua_kaligo/MainScreen.java
@@ -12,17 +12,83 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
 
 public class MainScreen extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+    private TextView textView, fullname;
+    FloatingActionButton postbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        postbutton = findViewById(R.id.postButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        // try here
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Customers");
+        userID = user.getUid();
+
+        final TextView greetingTextView = (TextView) findViewById(R.id.greetings);
+        final TextView full_nameTextview = (TextView) findViewById(R.id.names);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Customer userprofile = snapshot.getValue(Customer.class);
+
+                Calendar c = Calendar.getInstance();
+                int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+                String fullname = userprofile.fullNames;
+
+                if(timeOfDay >= 0 && timeOfDay < 12){
+                    greetingTextView.setText("Good Morning ");
+                    full_nameTextview.setText(fullname+"!");
+                }else if(timeOfDay >= 12 && timeOfDay < 16){
+                    greetingTextView.setText("Good Afternoon ");
+                    full_nameTextview.setText(fullname+"!");
+                }else if(timeOfDay >= 16 && timeOfDay < 21){
+                    greetingTextView.setText("Good Evening " );
+                    full_nameTextview.setText(fullname+"!");
+                }else if(timeOfDay >= 21 && timeOfDay < 24){
+                    greetingTextView.setText("Good Night ");
+                    full_nameTextview.setText(fullname+"!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //if(mAuth.getCurrentUser().equals(rootRef.child("Customers"))){
+           // postbutton.setVisibility(View.INVISIBLE);
+        //}
+        //if(mAuth.getCurrentUser().getUid().equals("Customers")){
+          //  postbutton.setVisibility(View.INVISIBLE);
+        //}
+
 
         bottomNavigationView = findViewById(R.id.navigation);
 
