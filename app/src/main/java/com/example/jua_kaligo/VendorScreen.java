a73115623255b14df3c4b7514609b0292a0ab297
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-08-29 19:24:40 +0300

    done

---
+++ app/src/main/java/com/example/jua_kaligo/VendorScreen.java
@@ -0,0 +1,14 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VendorScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_screen);
+    }
+}
