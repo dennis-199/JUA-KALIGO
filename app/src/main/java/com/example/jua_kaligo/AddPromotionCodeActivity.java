024422b277913535d64fb3fc6dedcc7cc6f27a25
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-10-17 10:46:27 +0300

    promotion sent to database

--- app/src/main/java/com/example/jua_kaligo/AddPromotionCodeActivity.java
+++ app/src/main/java/com/example/jua_kaligo/AddPromotionCodeActivity.java
@@ -1,24 +1,43 @@
 package com.example.jua_kaligo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DecimalFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class AddPromotionCodeActivity extends AppCompatActivity {
     private ImageButton backBtn;
     private EditText promoCodeEt, promoDescriptionEt, promoPriceEt, minimumOrderPriceEt;
     private TextView expireDateTv;
     private Button addBtn;
+
+    // firebase auth
+    FirebaseAuth firebaseAuth;
+    // progress dialog
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +52,13 @@ public class AddPromotionCodeActivity extends AppCompatActivity {
         minimumOrderPriceEt = findViewById(R.id.minimumOrderPriceEt);
         expireDateTv = findViewById(R.id.expireDateTv);
         addBtn = findViewById(R.id.addBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
 
         // handle click go back
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -53,10 +79,13 @@ public class AddPromotionCodeActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inputData();
 
             }
         });
     }
+
+
 
     private void datePickerDialog() {
         // get current date set on calender
@@ -86,4 +115,79 @@ public class AddPromotionCodeActivity extends AppCompatActivity {
         datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()-1000);
 
     }
+    private String description, promoCode, promoPrice, minimumOrderPrice, expireDate;
+    private void inputData() {
+        // input data
+        promoCode = promoCodeEt.getText().toString().trim();
+        description = promoDescriptionEt.getText().toString().trim();
+        promoPrice = promoPriceEt.getText().toString().trim();
+        minimumOrderPrice = minimumOrderPriceEt.getText().toString().trim();
+        expireDate = expireDateTv.getText().toString().trim();
+
+        // validate form data
+        if(TextUtils.isEmpty(promoCode)){
+            Toast.makeText(this, "Enter discount code...", Toast.LENGTH_SHORT).show();
+            return;// don't proceed further
+        }
+        if(TextUtils.isEmpty(description)){
+            Toast.makeText(this, "Enter description...", Toast.LENGTH_SHORT).show();
+            return;// don't proceed further
+        }
+        if(TextUtils.isEmpty(promoPrice)){
+            Toast.makeText(this, "Enter promotion price ..", Toast.LENGTH_SHORT).show();
+            return;// don't proceed further
+        }
+        if(TextUtils.isEmpty(minimumOrderPrice)){
+            Toast.makeText(this, "Enter minimum order price  ..", Toast.LENGTH_SHORT).show();
+            return;// don't proceed further
+        }
+        if(TextUtils.isEmpty(expireDate)){
+            Toast.makeText(this, "Choose expired date  ..", Toast.LENGTH_SHORT).show();
+            return;// don't proceed further
+        }
+
+        // all fields entered add to db
+        addDataToDb();
+    }
+
+    private void addDataToDb() {
+        // all fields entered , show progress bar
+        progressDialog.setMessage("Adding Promotion Code...");
+        progressDialog.show();
+
+        String timestamp = ""+System.currentTimeMillis();
+        // setup data to add to db
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("id",""+timestamp);
+        hashMap.put("timestamp", ""+ timestamp);
+        hashMap.put("description", ""+ description);
+        hashMap.put("promoCode", ""+ promoCode);
+        hashMap.put("promoPrice", ""+ promoPrice);
+        hashMap.put("minimumOrderPrice", ""+ minimumOrderPrice);
+        hashMap.put("expireDate", ""+ expireDate);
+
+        // init db reference Users > current user > promotions > promo id > promo date
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseAuth.getUid()).child("Promotions").child(timestamp)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                        // code added
+                        progressDialog.dismiss();
+                        Toast.makeText(AddPromotionCodeActivity.this, "Promotion code added... ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // adding code failed
+                        progressDialog.dismiss();
+                        Toast.makeText(AddPromotionCodeActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
 }
