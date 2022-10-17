1164e6b276d4c2714bf19fa74caee9c48a4a65bf
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-10-17 08:30:02 +0300

    set expiry date

--- app/src/main/java/com/example/jua_kaligo/AddPromotionCodeActivity.java
+++ app/src/main/java/com/example/jua_kaligo/AddPromotionCodeActivity.java
@@ -2,13 +2,88 @@ package com.example.jua_kaligo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
 
 public class AddPromotionCodeActivity extends AppCompatActivity {
+    private ImageButton backBtn;
+    private EditText promoCodeEt, promoDescriptionEt, promoPriceEt, minimumOrderPriceEt;
+    private TextView expireDateTv;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_promotion_code);
+
+        // init ui views
+        backBtn = findViewById(R.id.backBtn);
+        promoCodeEt = findViewById(R.id.promoCodeEt);
+        promoDescriptionEt = findViewById(R.id.promoDescriptionEt);
+        promoPriceEt = findViewById(R.id.promoPriceEt);
+        minimumOrderPriceEt = findViewById(R.id.minimumOrderPriceEt);
+        expireDateTv = findViewById(R.id.expireDateTv);
+        addBtn = findViewById(R.id.addBtn);
+
+        // handle click go back
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        // handle click, pick date
+        expireDateTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog();
+
+            }
+        });
+        // handle click , add promotion code to firebase db
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void datePickerDialog() {
+        // get current date set on calender
+        Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                DecimalFormat mFormat = new DecimalFormat("00");
+                String pDay = mFormat.format(dayOfMonth);
+                String pMonth = mFormat.format(monthOfYear);
+                String pYear = ""+year;
+                String pDate = pDay + "/"+pMonth+"/"+pYear;// eg 27/04/2022
+
+                expireDateTv.setText(pDate);
+
+
+            }
+        }, mYear, mMonth, mDay);
+        // show dialog
+        datePickerDialog.show();
+        // disable past days section
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()-1000);
+
     }
 }
