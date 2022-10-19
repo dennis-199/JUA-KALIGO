3280d67654876fe5c91a4ac742326305f022d52f
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-10-19 14:56:05 +0300

    mpesa working good

--- app/src/main/java/com/example/jua_kaligo/MpesaActivity.java
+++ app/src/main/java/com/example/jua_kaligo/MpesaActivity.java
@@ -1,5 +1,12 @@
 package com.example.jua_kaligo;
 
+import static com.example.jua_kaligo.utils.AppConstants.BUSINESS_SHORT_CODE;
+import static com.example.jua_kaligo.utils.AppConstants.CALLBACKURL;
+import static com.example.jua_kaligo.utils.AppConstants.PARTYB;
+import static com.example.jua_kaligo.utils.AppConstants.PASSKEY;
+import static com.example.jua_kaligo.utils.AppConstants.TRANSACTION_TYPE;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -10,15 +17,29 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.jua_kaligo.model.AccessToken;
+import com.example.jua_kaligo.model.STKPush;
 import com.example.jua_kaligo.utils.ApiClient;
+import com.example.jua_kaligo.utils.Utils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
-public class MpesaActivity extends AppCompatActivity{
+public class MpesaActivity extends AppCompatActivity implements View.OnClickListener{
     private ApiClient mApiClient;
     private ProgressDialog mProgressDialog;
     private ImageButton backBtn;
+    private FirebaseAuth firebaseAuth;
 
     @BindView(R.id.etAmount)
     EditText mAmount;
@@ -33,11 +54,41 @@ public class MpesaActivity extends AppCompatActivity{
         setContentView(R.layout.activity_mpesa);
 
         ButterKnife.bind(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()){
+                            String phone = "" + ds.child("phone").getValue();
+
+                            String phoneno = phone.replace("+254","0");
+                            mPhone.setText(phoneno);
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
         String amount = getIntent().getStringExtra("orderCost");
-        mAmount.setText(amount);
+        String amount2 = amount.replace("ksh","");
+        mAmount.setText(amount2);
 
         backBtn = findViewById(R.id.backBtn);
+
+        mProgressDialog = new ProgressDialog(this);
+        mApiClient = new ApiClient();
+        mApiClient.setIsDebug(true); //Set True to enable logging, false to disable.
+
+        mPay.setOnClickListener(this);
+
+        getAccessToken(); // Request for an access token from the MPESA API.
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +96,84 @@ public class MpesaActivity extends AppCompatActivity{
                 onBackPressed();
             }
         });
+    }
+
+    private void getAccessToken() {
+        mApiClient.setGetAccessToken(true);
+        mApiClient.mpesaService().getAccessToken().enqueue(new Callback<AccessToken>() {
+            @Override
+            public void onResponse(@NonNull Call<AccessToken> call, @NonNull Response<AccessToken> response) {
+
+                if (response.isSuccessful()) {
+                    mApiClient.setAuthToken(response.body().accessToken);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<AccessToken> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+    // Initiate payment after a user has entered their name and the amount they wish to pay.
+    // The amount can also be passed in from the price of a product for example, if you were integrating an online shop.
+    @Override
+    public void onClick(View view) {
+        if (view== mPay){
+            String phone_number = mPhone.getText().toString();
+            String amount = mAmount.getText().toString();
+            performSTKPush(phone_number,amount);
+        }
+    }
+    //These are the same details that we were setting when we introduced ourselves to the MPesa Daraja API.
+    public void performSTKPush(String phone_number,String amount) {
+        mProgressDialog.setMessage("Processing your request");
+        mProgressDialog.setTitle("Please Wait...");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.show();
+        String timestamp = Utils.getTimestamp();
+        STKPush stkPush = new STKPush(
+                BUSINESS_SHORT_CODE,
+                Utils.getPassword(BUSINESS_SHORT_CODE, PASSKEY, timestamp),
+                timestamp,
+                TRANSACTION_TYPE,
+                String.valueOf(amount),
+                Utils.sanitizePhoneNumber(phone_number),
+                PARTYB,
+                Utils.sanitizePhoneNumber(phone_number),
+                CALLBACKURL,
+                "test", //The account reference
+                "test"  //The transaction description
+        );
+
+        mApiClient.setGetAccessToken(false);
+
+        //Sending the data to the Mpesa API, remember to remove the logging when in production.
+        mApiClient.mpesaService().sendPush(stkPush).enqueue(new Callback<STKPush>() {
+            @Override
+            public void onResponse(@NonNull Call<STKPush> call, @NonNull Response<STKPush> response) {
+                mProgressDialog.dismiss();
+                try {
+                    if (response.isSuccessful()) {
+                        Timber.d("post submitted to API. %s", response.body());
+                    } else {
+                        Timber.e("Response %s", response.errorBody().string());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<STKPush> call, @NonNull Throwable t) {
+                mProgressDialog.dismiss();
+                Timber.e(t);
+            }
+        });
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/com/example/jua_kaligo/utils/AppConstants.java
+++ app/src/main/java/com/example/jua_kaligo/utils/AppConstants.java
@@ -13,7 +13,7 @@ public class AppConstants {
     public static final String BUSINESS_SHORT_CODE = "174379";
     public static final String PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
     public static final String TRANSACTION_TYPE = "CustomerPayBillOnline";
-    public static final String PARTYB = "174379"; // This is usually the same as the shortcode provided above.
+    public static final String PARTYB = "174379"; //  This is usually the same as the shortcode provided above.
     public static final String CALLBACKURL = "https://mydomain.com/path";
 
 }
