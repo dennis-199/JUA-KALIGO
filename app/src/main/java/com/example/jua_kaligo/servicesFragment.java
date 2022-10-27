89f9ddd8537e9fc8ab29b4366f6b85a416d9ad70
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-10-27 08:46:36 +0300

    services showing

--- app/src/main/java/com/example/jua_kaligo/servicesFragment.java
+++ app/src/main/java/com/example/jua_kaligo/servicesFragment.java
@@ -2,63 +2,73 @@ package com.example.jua_kaligo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link servicesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
 public class servicesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public servicesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment servicesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static servicesFragment newInstance(String param1, String param2) {
-        servicesFragment fragment = new servicesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private RecyclerView shopsRv, ordersRv;
+    private FirebaseAuth firebaseAuth;
+    private ArrayList<Vendor> shopsList;
+    private AdapterShop adapterShop;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_services, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance ();
+        //loadshopdetails();
+        shopsRv = (RecyclerView)rootView.findViewById(R.id.shopsRv);
+
+
+        shopsList = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("accountType").equalTo("Vendors")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        shopsList.clear();
+                        for(DataSnapshot ds: dataSnapshot.getChildren()){
+                            String typeOfBusiness = "" + ds.child("gender").getValue();
+                            Vendor modelShop = ds.getValue(Vendor.class);
+
+                            String shopCity = ""+ds.child("city").getValue();
+
+                             if (typeOfBusiness.equals("Services")){
+                            shopsList.add(modelShop);
+                             }
+
+                            // if you want to see all shops, skip the if statement and this
+                            // shopsList.add(modelShop);
+                        }
+                        adapterShop = new AdapterShop(getActivity(), shopsList);
+
+                        shopsRv.setAdapter(adapterShop);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        return rootView;
     }
 }
