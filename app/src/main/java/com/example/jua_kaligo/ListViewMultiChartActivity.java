8f0b18905e3d8a9f3561b98fcaaf156a0fa8ab1f
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-11-20 17:06:53 +0300

    list view

---
+++ app/src/main/java/com/example/jua_kaligo/ListViewMultiChartActivity.java
@@ -0,0 +1,14 @@
+package com.example.jua_kaligo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListViewMultiChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_multi_chart);
+    }
+}
