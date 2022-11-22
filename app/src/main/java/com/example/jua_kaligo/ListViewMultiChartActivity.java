b0beca4c2caa178c4e4cf8d583cdfad7d93eb5c5
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-11-22 09:24:24 +0300

    chart item

--- app/src/main/java/com/example/jua_kaligo/ListViewMultiChartActivity.java
+++ app/src/main/java/com/example/jua_kaligo/ListViewMultiChartActivity.java
@@ -1,14 +1,57 @@
 package com.example.jua_kaligo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListViewMultiChartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_list_view_multi_chart);
+        setTitle("ListViewMultiChartActivity");
+
+        ListView lv = findViewById(R.id.listView1);
+
+        ArrayList<ChartItem> list = new ArrayList<>();
+    }
+
+    private class ChartDataAdapter extends ArrayAdapter<ChartItem> {
+
+        ChartDataAdapter(Context context, List<ChartItem> objects) {
+            super(context, 0, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            //noinspection ConstantConditions
+            return getItem(position).getView(position, convertView, getContext());
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // return the views type
+            ChartItem ci = getItem(position);
+            return ci != null ? ci.getItemType() : 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 3; // we have 3 different item-types
+        }
     }
 }
