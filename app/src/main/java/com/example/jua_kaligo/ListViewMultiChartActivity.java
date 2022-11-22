c11298d6417ea514f6051a4ba97faec7fd7100b6
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-11-22 10:35:24 +0300

    update

--- app/src/main/java/com/example/jua_kaligo/ListViewMultiChartActivity.java
+++ app/src/main/java/com/example/jua_kaligo/ListViewMultiChartActivity.java
@@ -4,12 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +43,21 @@ public class ListViewMultiChartActivity extends AppCompatActivity {
         ListView lv = findViewById(R.id.listView1);
 
         ArrayList<ChartItem> list = new ArrayList<>();
+
+        // 30 items
+        for (int i = 0; i < 30; i++) {
+
+            if(i % 3 == 0) {
+                list.add(new LineChartItem(generateDataLine(i + 1), getApplicationContext()));
+            } else if(i % 3 == 1) {
+                list.add(new BarChartItem(generateDataBar(i + 1), getApplicationContext()));
+            } else if(i % 3 == 2) {
+                list.add(new PieChartItem(generateDataPie(), getApplicationContext()));
+            }
+        }
+
+        ChartDataAdapter cda = new ChartDataAdapter(getApplicationContext(), list);
+        lv.setAdapter(cda);
     }
 
     private class ChartDataAdapter extends ArrayAdapter<ChartItem> {
@@ -54,4 +85,78 @@ public class ListViewMultiChartActivity extends AppCompatActivity {
             return 3; // we have 3 different item-types
         }
     }
+
+    private LineData generateDataLine(int cnt) {
+
+        ArrayList<Entry> values1 = new ArrayList<>();
+
+        for (int i = 0; i < 28; i++) {
+            values1.add(new Entry(i, (int) (Math.random() * 65) + 40));
+        }
+
+        LineDataSet d1 = new LineDataSet(values1, "Completed orders " + "" + ", (1)");
+        d1.setLineWidth(2.5f);
+        d1.setCircleRadius(4.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> values2 = new ArrayList<>();
+
+        for (int i = 0; i < 28; i++) {
+            values2.add(new Entry(i, values1.get(i).getY() - 30));
+        }
+
+        LineDataSet d2 = new LineDataSet(values2, "Cancelled orders " + "" + ", (2)");
+        d2.setLineWidth(2.5f);
+        d2.setCircleRadius(4.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.MATERIAL_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.MATERIAL_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<ILineDataSet> sets = new ArrayList<>();
+        sets.add(d1);
+        sets.add(d2);
+
+        return new LineData(sets);
+    }
+
+    private BarData generateDataBar(int cnt) {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            entries.add(new BarEntry(i, (int) (Math.random() * 70) + 30));
+        }
+
+        BarDataSet d = new BarDataSet(entries, "New DataSet " + cnt);
+        d.setColors(ColorTemplate.MATERIAL_COLORS);
+        d.setHighLightAlpha(255);
+
+        BarData cd = new BarData(d);
+        cd.setBarWidth(0.9f);
+        return cd;
+    }
+
+    private PieData generateDataPie() {
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * 70) + 30), "Quarter " + (i+1)));
+        }
+
+        PieDataSet d = new PieDataSet(entries, "");
+
+        // space between slices
+        d.setSliceSpace(2f);
+        d.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        return new PieData(d);
+    }
+
+
+
+
+
 }
